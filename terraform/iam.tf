0c4a90b0eedc89c9d7fb1a07c8815f39e7df636d
marcello-clearcust <skydr4g0n@gmail.com>
2026-07-16 19:08:56 +0200

    Remove unnecessary terraform-refresh workflow and restore minimal IAM policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -44,22 +44,6 @@ resource "aws_iam_role_policy" "github_actions" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "TerraformState"
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject"
-        ]
-        Resource = "arn:aws:s3:::jpv-terraform-state/*"
-      },
-      {
-        Sid      = "TerraformStateBucket"
-        Effect   = "Allow"
-        Action   = "s3:ListBucket"
-        Resource = "arn:aws:s3:::jpv-terraform-state"
-      },
-      {
         Sid      = "ECRAuth"
         Effect   = "Allow"
         Action   = "ecr:GetAuthorizationToken"
